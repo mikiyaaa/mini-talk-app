3cb80f17188eb3b017bd79d8af4a7ae5dd4f488a
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-04-22 22:21:45 +0900

    mini-talk-appの準備

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,13 @@
+class MessagesController < ApplicationController
+  def new
+    @messages = Message.all
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(text: params[:message][:text])
+    @message.save
+  end
+
+
+end
